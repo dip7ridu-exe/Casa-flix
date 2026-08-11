7cc249fb951bfb95da091f9a7cc7cc07de2693c5
dip7ridu-exe <paulofada500@gmail.com>
2026-08-11 20:23:01 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="resenhaflix-shell-v28";
+const CACHE="resenhaflix-shell-v29";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icons/resenhaflix-logo.png","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
